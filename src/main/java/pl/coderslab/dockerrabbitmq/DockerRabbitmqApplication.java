8f692062d3ed38543269cb7efc25308c9400312f
MagRaj000 <mrajtor@visa.com>
2025-05-16 11:51:16 +0200

    first commit

---
+++ src/main/java/pl/coderslab/dockerrabbitmq/DockerRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dockerrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRabbitmqApplication.class, args);
+    }
+
+}
